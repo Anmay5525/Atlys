1980f34735ddac2a2e96d7d0dcc9b0ef5ab1d582
Anmay Kumar <anmay.kumar@razorpay.com>
2023-09-21 23:08:53 +0530

    update mock data timestamps

--- src/mock.ts
+++ src/mock.ts
@@ -4,7 +4,7 @@ import UserTwo from './assets/user_2_thumbnail.svg';
 export const mockFeed = [
   {
     userName: 'Theresa Webb',
-    postedAt: Date.now(),
+    postedAt: Date.now() - 10 * 1000,
     description:
       'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
     comments: ['world', 'hello'],
@@ -14,7 +14,7 @@ export const mockFeed = [
   },
   {
     userName: 'Marvin McKinney',
-    postedAt: Date.now(),
+    postedAt: Date.now() - 20 * 1000,
     description:
       'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
     comments: ['world'],
